51a6a82a0971b45acff2aa6c84bcdda92f85a5be
Vagner Cardozo de Almeida <vagnercardozo2002@gmail.com>
2024-07-07 12:18:40 -0300

    feat: Removida variavel que não será utilizada

--- api/src/main/config/router.php
+++ api/src/main/config/router.php
@@ -5,31 +5,30 @@ namespace Main\Config;
 class Router
 {
 
-    public static function get(string $path, string $controller, array $middlewares = [])
+    public static function get(string $path, string $controller)
     {
-        self::handleResponse($path, $controller, 'GET', $middlewares);
+        self::handleResponse($path, $controller, 'GET');
     }
 
-    public static function post(string $path, string $controller, array $middlewares = [])
+    public static function post(string $path, string $controller)
     {
-        self::handleResponse($path, $controller, 'POST', $middlewares);
+        self::handleResponse($path, $controller, 'POST');
     }
 
-    public static function put(string $path, string $controller, array $middlewares = [])
+    public static function put(string $path, string $controller)
     {
-        self::handleResponse($path, $controller, 'PUT', $middlewares);
+        self::handleResponse($path, $controller, 'PUT');
     }
 
-    public static function delete(string $path, string $controller, array $middlewares = [])
+    public static function delete(string $path, string $controller)
     {
-        self::handleResponse($path, $controller, 'DELETE', $middlewares);
+        self::handleResponse($path, $controller, 'DELETE');
     }
 
-    private static function handleResponse(string $route, mixed $controller, string $method, array $middlewares)
+    private static function handleResponse(string $route, mixed $controller, string $method)
     {
         print($route);
         print($method);
         print($controller);
-        print_r($middlewares);
     }
 }
