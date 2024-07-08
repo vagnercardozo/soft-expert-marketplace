fecea367ebbfa51173beda8ac255f15f72d6b58c
Vagner Cardozo de Almeida <vagnercardozo2002@gmail.com>
2024-07-08 11:59:22 -0300

    feat: Adicionado no repositorio a função show

--- api/src/infra/repositories/product-category.php
+++ api/src/infra/repositories/product-category.php
@@ -3,14 +3,21 @@
 namespace Src\Infra\Repositories\Postgres;
 
 use Src\Domain\Contract\Repositories\ProductCategory\IInsertProductCategory;
+use Src\Domain\Contract\Repositories\ProductCategory\IShowProductCategory;
 use Src\Infra\Repositories\Postgres\Models\ProductCategory;
 
-class ProductCategoryRepository implements IInsertProductCategory
+class ProductCategoryRepository implements IInsertProductCategory, IShowProductCategory
 {
   public function insert($data): ?ProductCategory
   {
     $productType = new ProductCategory;
     $productType->fill($data)->save();
     return $productType;
+  }
+
+  public function show($id): ?ProductCategory
+  {
+    $productType = new ProductCategory;
+    return $productType->whereId($id)->first();
   }
 }
