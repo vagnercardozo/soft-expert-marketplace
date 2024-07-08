c71d1d94463b77c156e811914d964d9647b0d3f8
Vagner Cardozo de Almeida <vagnercardozo2002@gmail.com>
2024-07-08 19:49:29 -0300

    feat: Adicionado controller update ProductCategory

---
+++ api/src/application/controllers/product-category/update-controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Src\Application\Controllers\ProductCategory;
+
+
+use Src\Application\Controllers\Controller;
+use Src\Domain\Entities\ProductCategory\IUpdateProductCategory;
+use Src\Application\Helpers\Response;
+use Throwable;
+
+class UpdateProductCategoryController extends Controller
+{
+  public function __construct(private IUpdateProductCategory $update)
+  {
+  }
+
+  protected function perform($request)
+  {
+    try {
+      $result = $this->update->setupUpdateProductCategory($request);
+      return Response::json($result, 200);
+    } catch (Throwable $error) {
+      return Response::json($error, 400);
+    }
+  }
+}
