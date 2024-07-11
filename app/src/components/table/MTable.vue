c13b0f73b9b586b07f8526de849b86f3327b8c18
Vagner Cardozo de Almeida <vagnercardozo2002@gmail.com>
2024-07-11 16:06:21 -0300

    feat: Adicionado componente de tabela

---
+++ app/src/components/table/MTable.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { QTableProps } from 'quasar';
+import { toRefs } from 'vue';
+
+const props = defineProps<{
+  rows: QTableProps['rows'];
+  columns: QTableProps['columns'];
+}>();
+
+const { rows, columns } = toRefs(props);
+</script>
+
+<template>
+  <div class="q-pa-md">
+    <q-table flat bordered :rows="rows" :columns="columns" row-key="name">
+      <template v-slot:top>
+        <div class="row justify-center full-width">CADASTRO</div>
+      </template>
+      <template v-slot:header="props">
+        <q-tr :props="props" style="border: 1px solid red">
+          <q-th v-for="col in props.cols" :key="col.name" :props="props">
+            {{ col.label }}
+          </q-th>
+        </q-tr>
+      </template>
+
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td v-for="col in props.cols" :key="col.name" :props="props">
+            {{ col.value }}
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+  </div>
+</template>
