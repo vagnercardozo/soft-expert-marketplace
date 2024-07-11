5a8cb90c4b8c6fea4c1358593feeaf5b8d9326e1
Vagner Cardozo de Almeida <vagnercardozo2002@gmail.com>
2024-07-11 16:26:35 -0300

    feat: Adicionado componente de botão

---
+++ app/src/components/buttons/MButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { StyleValue, toRefs } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    backgroundColor?: string;
+    textColor: string;
+    label: string;
+    style?: StyleValue;
+  }>(),
+  { backgroundColor: 'primary', textColor: 'white' }
+);
+
+const { backgroundColor, textColor, label } = toRefs(props);
+</script>
+
+<template>
+  <q-btn
+    :color="backgroundColor"
+    :text-color="textColor"
+    :label="label"
+    :style="style"
+  />
+</template>
