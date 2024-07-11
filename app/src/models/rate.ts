50714ec1f63d2988d659bd8ed977871c97f9480c
Vagner Cardozo de Almeida <vagnercardozo2002@gmail.com>
2024-07-11 16:07:07 -0300

    feat: Adicionada implementação da tabela

---
+++ app/src/models/rate.ts
@@ -0,0 +1,54 @@
+import { QTableProps } from 'quasar';
+import { useDate } from 'src/helpers/dates';
+
+const { formatDateBR } = useDate();
+
+export type Rate = {
+  id: number;
+  value: number;
+  description: string;
+  created_at: string | Date;
+  updated_at: string | Date;
+};
+
+export const columnsRate: QTableProps['columns'] = [
+  {
+    name: 'id',
+    label: 'ID',
+    format: (v: string) => v ?? '-',
+    field: 'id',
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'value',
+    label: 'Valor',
+    field: 'value',
+    format: (v: number) => (v ? String(v).concat('%') : '-'),
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'description',
+    label: 'Descrição',
+    field: 'description',
+    format: (v: string) => v ?? '-',
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'updated_at',
+    label: 'Atualizado Em',
+    field: 'updated_at',
+    format: (val: string) => (val ? formatDateBR(val) : '-'),
+    align: 'left',
+    sortable: true,
+  },
+  {
+    name: 'actions',
+    label: 'Ações',
+    sortable: false,
+    field: 'id',
+    align: 'right',
+  },
+];
--- app/src/pages/rate/RateView.vue
+++ app/src/pages/rate/RateView.vue
@@ -1,5 +1,33 @@
-<template>
-  <q-page class="row items-center justify-evenly"> </q-page>
-</template>
+<script setup lang="ts">
+import MTable from 'src/components/table/MTable.vue';
+import { columnsRate, Rate } from 'src/models/rate';
+import { ref } from 'vue';
 
-<script setup lang="ts"></script>
+const rows = ref<Rate[]>([
+  {
+    id: 1,
+    description: 'ICMS',
+    value: 2.5,
+    created_at: '2024-07-11',
+    updated_at: '2024-07-11',
+  },
+  {
+    id: 2,
+    description: 'TAXA',
+    value: 10,
+    created_at: '2024-07-11',
+    updated_at: '2024-07-11',
+  },
+]);
+</script>
+
+<template>
+  <q-page>
+    <div class="title flex justify-center">
+      <p class="text-h3 q-ma-xl">Taxas</p>
+    </div>
+    <div>
+      <MTable :columns="columnsRate" :rows="rows" />
+    </div>
+  </q-page>
+</template>
