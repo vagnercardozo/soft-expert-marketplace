38b05efd3f52ef5c5c705d7fac609df24aa2df7f
Vagner Cardozo de Almeida <vagnercardozo2002@gmail.com>
2024-07-12 00:02:44 -0300

    feat: Adicionado componente de input

---
+++ app/src/components/inputs/MInput.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const model = ref<string | number | null>(null);
+</script>
+
+<template>
+  <q-input outlined class="q-mb-md" hide-bottom-space v-model="model" />
+</template>
